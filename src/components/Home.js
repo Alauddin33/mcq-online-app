f5fb1e79a124067360f9ab979c237def8a84f668
Alauddin33 <shamem33@gmail.com>
2022-10-12 22:44:39 +0600

    FaQ added

--- src/components/Home.js
+++ src/components/Home.js
@@ -28,8 +28,8 @@ const Home = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-8 h-8 mb-8 dark:text-violet-400">
                             <path fillRule="evenodd" d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
                         </svg>
-                        <h2 className="text-3xl font-semibold leading-none">Modern solutions to all kinds of problems</h2>
-                        <p className="mt-4 mb-8 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non voluptatum rem amet!</p>
+                        <h2 className="text-3xl font-semibold leading-none">Importance of perticipating quiz:</h2>
+                        <p className="mt-4 mb-8 text-md">Online quizzes are a great way of assessing your knowledge. Various platforms conduct online quizzes related to several subjects and verticals. Quizzing is always engaging for anyone, irrespective of their age. It is an engaging way to gain knowledge. Therefore, quizzes must be included in the assessment schedule of every student.</p>
                         <button className="self-start px-10 py-3 text-lg font-medium rounded-3xl dark:bg-violet-400 dark:text-gray-900">Get started</button>
                     </div>
                 </div>
